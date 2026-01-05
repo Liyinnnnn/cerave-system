0cee88a159b0378e898f18a2780a0f5845257e43
Yeap Li Yin <yinnyliyin193@gmail.com>
2026-01-05 08:38:00 +0800

    Remove problematic request()-based proxy trust from bootrely on middleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
-use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,16 +22,6 @@ class AppServiceProvider extends ServiceProvider
     {
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
-
-            // Trust common forwarded headers when behind Railway's proxy
-            request()->setTrustedProxies(
-                ['*'],
-                SymfonyRequest::HEADER_X_FORWARDED_FOR
-                    | SymfonyRequest::HEADER_X_FORWARDED_HOST
-                    | SymfonyRequest::HEADER_X_FORWARDED_PORT
-                    | SymfonyRequest::HEADER_X_FORWARDED_PROTO
-                    | SymfonyRequest::HEADER_X_FORWARDED_PREFIX
-            );
         }
     }
 }
